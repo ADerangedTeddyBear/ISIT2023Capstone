ca14806ab5ad2e49776af74535597b9bd06b500a
peverall <code.statera@gmail.com>
2023-03-10 12:14:18 -0800

    logo and 2 links added. Next link functionality will be added to take user to scan and home page

--- the-directory_react_frontend/src/App.js
+++ the-directory_react_frontend/src/App.js
@@ -1,10 +1,49 @@
-import logo from './logo.svg';
+//import React from 'react';
+//import logo from './logo.svg';
+import logo from './assets/images/crocLogo.png'
+import Nav from './components/page_components/Nav';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    
+    <div className='navbar-Container'>
+
+      <nav>
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />          
+        </div>
+
+        <div className="App-nav">
+          <Nav />
+        </div>
+        
+      </nav>
+
+      
+    
+   
+    </div>
+
+
+
+
+
+
+  );
+}
+
+export default App;
+
+
+//<img src={logo} className="App-logo" alt="logo" />
+
+//<Nav />
+
+
+/*Boiler plate code
+   <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +57,5 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
 
-export default App;
+      */
---
+++ the-directory_react_frontend/src/components/page_components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/styles/Nav.css';
+
+export default function Nav(){
+    return (
+        <div className='nav_bar'>
+                
+                    <a href="#home">Home</a>
+                    <a href="#scan">Scan</a>
+        </div>
+    )
+}
