a4b3a6a52bd169ab734d8f27d000ba6a213cc5a9
ADerangedTeddyBear <cunhivu@gmail.com>
2023-04-09 14:19:47 -0700

    Added browser-mobile distinction in Front-end code

--- the-directory_react_frontend/src/components/page_components/Nav.js
+++ the-directory_react_frontend/src/components/page_components/Nav.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import '../../assets/styles/Nav.css';
 import { Link } from 'react-router-dom';
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect'; 
 
 export default function Nav(){
     return (
         <div className='nav_bar'>
+            
+            {/* Browser */}
+            <BrowserView>
+                <Link className = 'link_item' to="/">Home</Link>
+                <Link  className = 'link_item' to="/scan">Scan</Link>
+            </BrowserView>
 
-            <Link className = 'link_item' to="/">Home</Link>
-
-            <Link  className = 'link_item' to="/scan">Scan</Link>
-
-            <Link  className = 'link_item' to="/scanner">Scanner</Link>
-
-
-
+            {/* Mobile */}
+            <MobileView>
+                <Link className = 'link_item' to="/">Home</Link>
+                <Link  className = 'link_item' to="/scan">Scan</Link>
+                <Link  className = 'link_item' to="/scanner">Scanner</Link>
+            </MobileView>
         </div>
     )
 }
