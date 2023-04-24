a2a4e91ab2ff5200ab6cbc0b0cf927edcd5ae2a6
ADerangedTeddyBear <cunhivu@gmail.com>
2023-04-23 17:54:46 -0700

    Added ScanItem component

---
+++ the-directory_react_frontend/src/components/page_components/ScanItem.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import '../../assets/styles/Nav.css';
+import '../../assets/styles/W3.css';
+
+import { Link } from 'react-router-dom';
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect'; 
+
+export default function ScanItem(props){
+    return (
+        <div>
+            
+            {/* Browser */}
+            <BrowserView>
+                <div className="w3-container w3-mobile" style={{marginTop: '15px', marginBotto: '15px'}}>
+                    <div className="w3-card-4 w3-border">
+                        <header className="w3-container w3-white">
+                            <h1>{props.itemName}</h1>
+                        </header>
+
+                        <div className="w3-container">
+                            <p>{props.itemDescription}</p>
+                        </div>
+                        <br></br>
+                        <div className="w3-container">
+                            Link: {props.itemLink}
+                        </div>
+                    </div>
+                </div>
+                <br></br>
+            </BrowserView>
+
+            {/* Mobile */}
+            <MobileView>
+                <div className="w3-container w3-mobile" style={{marginTop: '5px', marginBotto: '5px'}}>
+                    <div className="w3-card-4 w3-border">
+                        <header className="w3-container w3-white">
+                            <h1>{props.itemName}</h1>
+                        </header>
+
+                        <div className="w3-container">
+                            <p>{props.itemDescription}</p>
+                        </div>
+                        <br></br>
+                        <div className="w3-container">
+                            Link: {props.itemLink}
+                        </div>
+                    </div>
+                </div>
+                <br></br>
+            </MobileView>
+        </div>
+    )
+}
