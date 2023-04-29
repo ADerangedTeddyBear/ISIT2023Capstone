cb2a1a8cd173d6021180200a573f41b2c2e25f46
chitalu7 <chitalu.am@bellevuecollege.edu>
2023-04-29 12:35:03 -0700

    Added frontend pagination

--- the-directory_react_frontend/src/pages/Scan.js
+++ the-directory_react_frontend/src/pages/Scan.js
@@ -1,79 +1,107 @@
 import ReactPaginate from 'react-paginate';
 import { useEffect, useState } from 'react';
 import '../assets/styles/Styles.css';
-import ScanItem from '../components/page_components/ScanItem';
+
 
 export default function Scan(){
-    const [testStuff, setTestStuff] = useState([]);
 
+    const [products, setProducts] = useState([]);
+    const [pageLimit, setPageLimit] = useState([]);
 
-    useEffect ( () => {
-        const testStuff = async () => {
+    useEffect(() => {
+        
+        const getProducts = async () => {
             const res = await fetch(
-              `http://localhost:1625/api/product`
+                'https://localhost:7294/api/Product/1'
             );
+
             const data = await res.json();
-            //const total = res.headers.get('x-total-count');
-            //setpageCount(Math.ceil(total/limit));
-            //console.log(Math.ceil(total/12));
-            setTestStuff(data);
-          };
-      
-          testStuff();
-      
-        }, []);
 
-        /*
-        fetch('http://localhost:1625/api/product')
-        .then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-            throw new Error ("Server not up or malfunctioning");
-        })
-        .then((res) => {
-            setTestStuff(res); 
-            console.log(res); 
-            console.log(testStuff)
-        })
-        .catch((err) => console.log(err));
+            setProducts(data.products);
+            setPageLimit(data.pages);
+            console.log("The limit is " + data.pages);           
+        }
 
-        }, [])*/
+        getProducts();
+
+
+    }, []);
+
+
+    const fetchProducts = async (currentPage) => {
+
+        const requestURL = "https://localhost:7294/api/Product/" + currentPage;
+        const res = await fetch(
+            requestURL
+
+        );
+
+        const data = await res.json();
+        
+        return (data.products);
+    };
+
+
+    const handlePageClick = async (data) =>{
+
+        let currentPage = data.selected + 1;
+
+        const productsFormsServer = await fetchProducts(currentPage);
+
+        setProducts(productsFormsServer);
+    }
 
     return (
-        <div className="container">
-        <div className="row m-2">
-     
-        {testStuff.map((item) => {
-        return (
-            <div key={item.product_id}>
-                <ScanItem itemName = {item.product_name} itemDescription ={item.discription} itemLink = {item.link_Url} />
-            </div>
-            );
-        })}
+        <div>
 
-        <ReactPaginate 
-            previousLabel={'Previous'}
-            nextLabel={'Next'}
-            breakLabel={'...'}
-            pageCount={10}
-            marginPagesDisplayed={2}
-            pageRangeDisplayed={3}
-            //onPageChange={handlePageClick}
-            containerClassName={'pagination justify-content-center'}
-            pageClassName={'page-item'}
-            pageLinkClassName={'page-link'}
-            previousClassName={'page-item'}
-            previousLinkClassName={'page-link'}
-            nextClassName={'page-item'}
-            nextLinkClassName={'page-link'}
-            breakClassName={'page-item'}
-            breakLinkClassName={'page-link'}
-            activeClassName={'active'}
-      />
+
+        <div className='container'>
+            <div className='row m-2'>
+
+                {products.map((product) => {
+                    return (
+                        <div key={product.id} className="col-sm-6 col-md-4 v my-2" >
+                            <div className="card shadow-sm w-100" style={{ minHeight: 225 }}>
+                                <div className="card-body">
+                                    <h5 className="card-title text-center h2">Product Name: {product.productName}</h5>
+                                    <h5 className="card-title text-center h2">Price: {product.imgSrc}</h5>
+                                    <p className="card-text">Description: {product.description}</p>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+
+                
+
+            </div>             
+        </div> 
+
+        <div>
+            <ReactPaginate 
+                previousLabel={'Previous'}
+                nextLabel={'Next'}
+                breakLabel={'...'}
+                pageCount={pageLimit}
+                marginPagesDisplayed={3}
+                pageRangeDisplayed={3}
+                onPageChange={handlePageClick}
+                containerClassName={'pagination justify-content-center'}
+                pageClassName={'page-item'}
+                pageLinkClassName={'page-link'}
+                previousClassName={'page-item'}
+                previousLinkClassName={'page-link'}
+                 nextClassName={'page-item'}
+                nextLinkClassName={'page-link'}
+                breakClassName={'page-item'}
+                breakLinkClassName={'page-link'}
+                activeClassName={'active'}
+            />
+        </div>      
+
+
         </div>
-        </div>
-    );
+    )
 }
 
 /*PREVIOUS CODE
