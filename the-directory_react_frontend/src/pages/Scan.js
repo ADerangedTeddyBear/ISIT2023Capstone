ff950f65b8d2162b10471241abcd86cd687b59e8
chitalu7 <chitalu.am@bellevuecollege.edu>
2023-05-08 15:45:56 -0700

    Added swiper and minimal css for testing style

--- the-directory_react_frontend/src/pages/Scan.js
+++ the-directory_react_frontend/src/pages/Scan.js
@@ -4,6 +4,15 @@ import { useEffect, useState } from 'react';
 import '../assets/styles/Styles.css';
 import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect'; 
 
+//Swiper Component imports
+//import Slider from '../components/page_components/Slider';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper';
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
+import { useRef } from 'react';
+//Swiper components END
 
 // Iamge_import
 import {
@@ -27,6 +36,11 @@ export default function Scan(){
     const [dictionary, setDictionary] = useState({});
 
     const [allProducts, setAllProducts] = useState([]);
+
+    
+    //Swiper payload
+    const [productPerPage, setProductPerPAge] = useState([]);
+    
     useEffect(() => {
         const getAllProducts = async () => {
             const res = await fetch(
@@ -58,14 +72,18 @@ export default function Scan(){
         }
         getAllProducts()
 
-
-        const getProducts = async () => {
+ const getProducts = async () => {
             const res = await fetch(
                 'https://localhost:7294/api/Pages/1'
             );
 
             const data = await res.json();
             setProducts(data.products);
+
+            //Swiper Payload
+            setProductPerPAge(JSON.parse(JSON.stringify(data)));
+
+        
 
             // Check Mondo DB
             // data.products.forEach((item)=>{
@@ -105,114 +123,233 @@ export default function Scan(){
         const data = await res.json();
         return (data.products);
     };
+    
+    const handlePageClickNext = async (data) =>{
+        // console.log("The Data page limit is: " + pageLimit); PASSED!!!
+        //console.log("The Data page limit is: " + products[0].id); PASSED!!!
+        //console.log("I AM THE LAST!!!"); //PASSED!!!
+        //console.log("The Data page is: " + productPerPage.currentPage); //PASSED!!!
 
-
-    const handlePageClick = async (data) =>{
-
-        let currentPage = data.selected + 1;
-
+        let currentPage = productPerPage.currentPage + 1;
         const productsFormsServer = await fetchProducts(currentPage);
-
         setProducts(productsFormsServer);
     }
 
+    const handlePageClickPrev = async (data) =>{
+        // console.log("The Data page limit is: " + pageLimit); PASSED!!!
+        //console.log("The Data page limit is: " + products[0].id); PASSED!!!
+        //console.log("I AM THE LAST!!!"); //PASSED!!!
+        //console.log("The Data page is: " + productPerPage.currentPage); //PASSED!!!
 
+        if (productPerPage.currentPage != 1)
+        {
+            let currentPage = productPerPage.currentPage - 1;
+            const productsFormsServer = await fetchProducts(currentPage);
+            setProducts(productsFormsServer);
+
+        }
+    
+    }
+
+
+     //Swiper Functions
+    const [slideBegOrNot, handleSlideByState] = useState({
+        isFirst: true,
+        isLast: false,
+    })
+    const SlideRef = useRef();
+
+    const handleNext = () => {        
+        SlideRef.current.swiper.slideNext();
+
+    }
+
+    const handlePrev = () => {
+        SlideRef.current.swiper.slidePrev();
+    }
+
+    const onSlideChange = swiper => {
+        handleSlideByState({
+            isFirst: swiper.isBeginning,
+            isLast: swiper.isEnd,
+        });
+    };
+
+    const printConsole = () => {
+        console.log("Reached the end of the scrolling");
+    }
+
+    const { isLast, isFirst } = slideBegOrNot;
+
+    //PREVIOUS HANDLER
+    // const handlePageClick = async (data) =>{
+
+    //     let currentPage = data.selected + 1;
+
+    //     const productsFormsServer = await fetchProducts(currentPage);
+
+    //     setProducts(productsFormsServer);
+    // }
+
+    //UPDATED RETURN CODE
     return (
         <div>
 
-        <BrowserView>
-        <div className='container'>
-            <div className='row m-2'>
-                {/* {console.log(dictionary)} */}
-                {/* {console.log(typeof(Object.keys(dictionary)))} */}
-                {/* {console.log(Object.values(dictionary))} */}
-                {products.map((product) => {
-                    {Object.entries(dictionary).map((dic) => {
-                        if (dic[0] == product.id){
-                            product.imageAccessNumber = dic[1]
-                            //console.log(dic[0], product.id, dic[1])
-                        }
-                    })}
-                    return (
-                        <div key={product.id}>
-                            <ScanItem itemName = {product.productName} itemDescription ={product.description} itemImageName = {product.imageAccessNumber}/>
-                        </div>
-                    );
-                })}
+            {/* Swiper Test BEGIN */}
+
+            <div className='swiper-paginate-controls'></div>
+            <div className="bs-icons">
+                <BsArrowLeft id='arrowLeft' onClick= {()=> {isFirst ? handlePageClickPrev(): handlePrev()}}/>
+                <BsArrowRight id='arrowRight'  onClick= {()=> {isLast ? handlePageClickNext(): handleNext()}}/>            
+
+
+                {/* <BsArrowLeft id='arrowLeft' className={`Arrow ${isFirst ? 'disabled': ''}`} onClick={handlePrev}/>
+                <BsArrowRight id='arrowRight' className={`Arrow ${isLast ? 'disabled': ''}`} onClick={handleNext}/> */}
             </div>
-        </div>
+                   
+            <div>
+                <Swiper
+                slidesPerView={1}
+                spaceBetween={0}
+                className={'mySwiper'}
+                ref={SlideRef}
+                onSlideChange={onSlideChange}
+                pagination={{
+                    el: '.swiper-paginate-controls',
+                    type: 'fraction',
+                }}
+                navigation={false}
+                modules={[Pagination, Navigation]}
+                >
+                        {products.map((product) => {
+                            {Object.entries(dictionary).map((dic) => {
+                                if (dic[0] == product.id){
+                                    product.imageAccessNumber = dic[1]
+                                    //console.log(dic[0], product.id, dic[1])
+                                }
+                            })}
 
-        <div>
-            <ReactPaginate
-                previousLabel={'Previous'}
-                nextLabel={'Next'}
-                breakLabel={'...'}
-                pageCount={pageLimit}
-                marginPagesDisplayed={3}
-                pageRangeDisplayed={3}
-                onPageChange={handlePageClick}
-                containerClassName={'pagination justify-content-center'}
-                pageClassName={'page-item'}
-                pageLinkClassName={'page-link'}
-                previousClassName={'page-item'}
-                previousLinkClassName={'page-link'}
-                 nextClassName={'page-item'}
-                nextLinkClassName={'page-link'}
-                breakClassName={'page-item'}
-                breakLinkClassName={'page-link'}
-                activeClassName={'active'}
-            />
-        </div>      
-        </BrowserView>
-        <MobileView>
-            {/* ReactPaginate Pending Removal */}
-        <div>
-            <ReactPaginate 
-                previousLabel={'Previous'}
-                nextLabel={'Next'}
-                breakLabel={'...'}
-                pageCount={pageLimit}
-                marginPagesDisplayed={3}
-                pageRangeDisplayed={3}
-                onPageChange={handlePageClick}
-                containerClassName={'pagination justify-content-center'}
-                pageClassName={'page-item'}
-                pageLinkClassName={'page-link'}
-                previousClassName={'page-item'}
-                previousLinkClassName={'page-link'}
-                 nextClassName={'page-item'}
-                nextLinkClassName={'page-link'}
-                breakClassName={'page-item'}
-                breakLinkClassName={'page-link'}
-                activeClassName={'active'}
-            />
-        </div>    
-        <div className='container'>
-            <div className='row m-2'>
+                            return (
+                                <SwiperSlide>              
+                        
+                                    <div className='row m-2'>
+                                        <div key={product.id}>
+                                            <ScanItem itemName = {product.productName} itemDescription ={product.description} itemImageName = {product.imageAccessNumber}/>
+                                        </div>                                
+                                    </div>                       
 
-                {products.map((product) => {
-                    return (
-                        <div key={product.id}>
-                            <ScanItem itemName = {product.productName} itemDescription ={product.description} itemImageName = {product.imageAccessNumber}/>
+                                </SwiperSlide>
+                                
+                            );
+                        })}
+                    
+                
+                </Swiper>
+            </div>
+
+                 {/* Swiper Test END */}    
+            
+                 </div>
+    )
+}
+
+    //PREVIOUS RETURN CODE
+    // return (
+    //     <div>
+
+    //     <BrowserView>
+    //     <div className='container'>
+    //         <div className='row m-2'>
+    //             {/* {console.log(dictionary)} */}
+    //             {/* {console.log(typeof(Object.keys(dictionary)))} */}
+    //             {/* {console.log(Object.values(dictionary))} */}
+    //             {products.map((product) => {
+    //                 {Object.entries(dictionary).map((dic) => {
+    //                     if (dic[0] == product.id){
+    //                         product.imageAccessNumber = dic[1]
+    //                         //console.log(dic[0], product.id, dic[1])
+    //                     }
+    //                 })}
+    //                 return (
+    //                     <div key={product.id}>
+    //                         <ScanItem itemName = {product.productName} itemDescription ={product.description} itemImageName = {product.imageAccessNumber}/>
+    //                     </div>
+    //                 );
+    //             })}
+    //         </div>
+    //     </div>
+
+    //     <div>
+    //         <ReactPaginate
+    //             previousLabel={'Previous'}
+    //             nextLabel={'Next'}
+    //             breakLabel={'...'}
+    //             pageCount={pageLimit}
+    //             marginPagesDisplayed={3}
+    //             pageRangeDisplayed={3}
+    //             onPageChange={handlePageClick}
+    //             containerClassName={'pagination justify-content-center'}
+    //             pageClassName={'page-item'}
+    //             pageLinkClassName={'page-link'}
+    //             previousClassName={'page-item'}
+    //             previousLinkClassName={'page-link'}
+    //              nextClassName={'page-item'}
+    //             nextLinkClassName={'page-link'}
+    //             breakClassName={'page-item'}
+    //             breakLinkClassName={'page-link'}
+    //             activeClassName={'active'}
+    //         />
+    //     </div>      
+    //     </BrowserView>
+    //     <MobileView>
+    //         {/* ReactPaginate Pending Removal */}
+    //     <div>
+    //         <ReactPaginate 
+    //             previousLabel={'Previous'}
+    //             nextLabel={'Next'}
+    //             breakLabel={'...'}
+    //             pageCount={pageLimit}
+    //             marginPagesDisplayed={3}
+    //             pageRangeDisplayed={3}
+    //             onPageChange={handlePageClick}
+    //             containerClassName={'pagination justify-content-center'}
+    //             pageClassName={'page-item'}
+    //             pageLinkClassName={'page-link'}
+    //             previousClassName={'page-item'}
+    //             previousLinkClassName={'page-link'}
+    //              nextClassName={'page-item'}
+    //             nextLinkClassName={'page-link'}
+    //             breakClassName={'page-item'}
+    //             breakLinkClassName={'page-link'}
+    //             activeClassName={'active'}
+    //         />
+    //     </div>    
+    //     <div className='container'>
+    //         <div className='row m-2'>
+
+    //             {products.map((product) => {
+    //                 return (
+    //                     <div key={product.id}>
+    //                         <ScanItem itemName = {product.productName} itemDescription ={product.description} itemImageName = {product.imageAccessNumber}/>
 
 
                           
-                        </div>
-                    );
-                })}
+    //                     </div>
+    //                 );
+    //             })}
 
                 
 
-            </div>             
-        </div>   
-        </MobileView>
+    //         </div>             
+    //     </div>   
+    //     </MobileView>
 
 
 
 
-        </div>
-    )
-}
+    //     </div>
+    // )
+
 
 /*PREVIOUS CODE
 import React, { useState, useEffect } from 'react';
