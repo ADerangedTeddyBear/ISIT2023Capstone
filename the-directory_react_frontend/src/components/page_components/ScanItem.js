50462e42411aea796c090d6b3b55ac3ca52a7e5c
ADerangedTeddyBear <cunhivu@gmail.com>
2023-05-14 10:25:43 -0700

    Additional scaling fixes.

--- the-directory_react_frontend/src/components/page_components/ScanItem.js
+++ the-directory_react_frontend/src/components/page_components/ScanItem.js
@@ -57,7 +57,7 @@ export default function ScanItem(props){
             {/* Mobile */}
             <MobileView>
                 <div className="w3-container MobileScanItem" style={{marginTop: '5px', marginBottom: '5px'}}>
-                    <div className="w3-card-4 w3-border MobileScanItem">
+                    <div className="w3-card-4 w3-border">
                         <header className="w3-container w3-white">
                             <h1 style={{fontSize: '6vw'}}>{props.itemName}</h1>
                         </header>
