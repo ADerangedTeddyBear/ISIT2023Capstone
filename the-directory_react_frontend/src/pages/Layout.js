81e3b79e3907458f538adab5153bc43e2a43ba9f
ADerangedTeddyBear <cunhivu@gmail.com>
2023-05-13 19:45:02 -0700

    Pinned mobile header to the top

--- the-directory_react_frontend/src/pages/Layout.js
+++ the-directory_react_frontend/src/pages/Layout.js
@@ -17,10 +17,11 @@ const Layout = () => {
     </BrowserView>
 
     <MobileView>
-      <h1 className="Title-label">The Directory</h1>
+      <h1 className="Title-label PageTitleMobile">The Directory</h1>
       {/* <img src={logo} className="App-logo-alt" alt="logo" /> */}
       <br></br>
       <Outlet />
+      <br></br>
       <Nav />
     </MobileView>
 
