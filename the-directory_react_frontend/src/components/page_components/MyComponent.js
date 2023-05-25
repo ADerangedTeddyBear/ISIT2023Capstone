232df837859b6bff5566b78de90dba345619f769
Jaegyeom Kim <jaegyeom1215@gmail.com>
2023-05-24 17:22:14 -0700

    Add comment to MyComponent.js
    Add comment to MyComponent.js

--- the-directory_react_frontend/src/components/page_components/MyComponent.js
+++ the-directory_react_frontend/src/components/page_components/MyComponent.js
@@ -7,38 +7,41 @@ const MyComponent = () => {
   const [data, setData] = useState([]);
   const {productId} = useParams();
 
+  // Get All Project and Save it to data
   useEffect(() => {
     axios.get('https://localhost:7294/api/Product')
     .then(response => setData(response.data))
     .catch(error => console.log(error));
     }, []);
 
-    useEffect(() => {
+  // Get the one Project which will display and save it to product
+  useEffect(() => {
       if (data.length > 0) {
         const selectedProduct = data.find((p) => p.sequences === productId);
         setProduct(selectedProduct);
       }
     }, [data, productId]);
 
-    
+
     // Test button Rout
     const [currentProjectId, setCurrentProjectId] = useState(1);
-
+    // Button Right 
     const handleNextProject = () => {
       const nextProjectId = currentProjectId === data.length ? 1 : currentProjectId + 1;
       setCurrentProjectId(nextProjectId);
     };
-  
+    // Button Left 
     const handlePreviousProject = () => {
       const previousProjectId = currentProjectId === 1 ? data.length : currentProjectId - 1;
       setCurrentProjectId(previousProjectId);
     };
 
+    // No URL params : set "/scan/2" 
     if (productId === undefined) {
-      console.log("Test")
       // Route parameter is empty, navigate to "scan/1"
       return <Navigate to="/scan/2" replace />;
     }
+
   return (
     <div>
       {/* Test button Rout */}
