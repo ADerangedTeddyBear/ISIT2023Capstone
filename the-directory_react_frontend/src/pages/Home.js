44eba9f6dfdbc8cc240a7e57eda9de368389be18
Jaegyeom Kim <jaegyeom1215@gmail.com>
2023-05-27 23:53:56 -0700

    Update Home.js
    Update Home.js

--- the-directory_react_frontend/src/pages/Home.js
+++ the-directory_react_frontend/src/pages/Home.js
@@ -1,69 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import { useState, useEffect } from "react";
-import {
-  ref,
-  getDownloadURL,
-  listAll,
-} from "firebase/storage";
+import { ref, getDownloadURL, listAll } from "firebase/storage";
 import { storage } from "../firebase";
 
-export default function Home(){
-    const [dictionary, setDictionary] = useState({});
-    const [allProducts, setAllProducts] = useState([]);
-    const imagesListRef = ref(storage, "images/");
-    const [data, setData] = useState([]);
-    
-    useEffect(() => {
-        axios.get('https://localhost:7294/api/Product')
-        .then(response => setData(response.data))
-        .catch(error => console.log(error));
+export default function Home() {
+  const [dictionary, setDictionary] = useState({});
+  const [allProducts, setAllProducts] = useState([]);
+  const imagesListRef = ref(storage, "images/");
+  const [data, setData] = useState([]);
 
-        const getAllProducts = async () => {
-            const res = await fetch(
-                'https://localhost:7294/api/Product'
-            );
-            const data = await res.json();
-            setAllProducts(data);
+  useEffect(() => {
+    axios.get('https://localhost:7294/api/Product')
+      .then(response => setData(response.data))
+      .catch(error => console.log(error));
 
-            listAll(imagesListRef).then((response) => {
-                response.items.forEach((item) => {
-                    getDownloadURL(item).then((url => {
-                            data.forEach((product)=>{
-                                if (product.imageAccessNumber == item.name){
-                                        setDictionary(prevDictionary => ({
-                                            ...prevDictionary,
-                                            [product.id]: url
-                                    }));
-                                }
-                            })
-                    }));
-                    });
-                });
-        }
-        getAllProducts()
-        }, []);
-    return (
-        <div>
-                {console.log(allProducts)}
-                {allProducts.map((product) => {
-                    {Object.entries(dictionary).map((dic) => {
-                        if (dic[0] == product.id){
-                            product.imageAccessNumber = dic[1]
-                        }
-                    })}
-                    return (
-                        <div key={product.id}>
-                            <a href={'http://localhost:3000/scan/' + product.id}>
-                                <h1>{product.productName}</h1>
-                                <img
-                                    className='itemImage'
-                                    src = {product.imageAccessNumber}                                
-                                />
-                            </a>
-                        </div>
-                    );
-                })}
-        </div>
-    )
+    const getAllProducts = async () => {
+      const res = await fetch('https://localhost:7294/api/Product');
+      const data = await res.json();
+      setAllProducts(data);
+
+      listAll(imagesListRef).then(response => {
+        const downloadPromises = response.items.map(item => {
+          return getDownloadURL(item).then(url => {
+            return { name: item.name, url };
+          });
+        });
+
+        Promise.all(downloadPromises).then(downloads => {
+          const imageDictionary = {};
+
+          data.forEach(product => {
+            downloads.forEach(download => {
+              if (product.imageAccessNumber === download.name) {
+                imageDictionary[product.id] = download.url;
+              }
+            });
+          });
+
+          setDictionary(imageDictionary);
+        });
+      });
+    }
+    getAllProducts();
+  }, []);
+
+  return (
+    <div>
+      {allProducts.map(product => {
+        console.log(product)
+        const imageUrl = dictionary[product.id];
+        return (
+          <div key={product.id}>
+            <a href={'http://localhost:3000/scan/' + product.sequences}>
+              <h1>{product.productName}</h1>
+              <img
+                className='itemImage'
+                src={imageUrl}
+                alt={product.productName}
+                style={{ width: '200px', height: '200px' }}
+              />
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
