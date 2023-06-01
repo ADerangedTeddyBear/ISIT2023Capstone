d43b51f370c796d39ac11c66e25908f3605d045a
ADerangedTeddyBear <cunhivu@gmail.com>
2023-05-31 21:56:40 -0700

    Converted card styling to my version

--- the-directory_react_frontend/src/components/Card.js
+++ the-directory_react_frontend/src/components/Card.js
@@ -11,7 +11,8 @@ const Card = ({ product }) => {
   };
 
   return (
-    <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={flipCard}>
+    <>
+        {/* <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={flipCard}>
       {console.log(product)}
 
       <div className="card-front">
@@ -25,7 +26,36 @@ const Card = ({ product }) => {
       <div className="card-back">
         <p>{description}</p>
       </div>
+    </div> */}
+
+{/* My version below */}
+    <div className="card-grid">
+
+<div className={`card ${isFlipped ? "flip" : ""}`}>
+
+  <div className="front" onClick={flipCard}>
+    <h1>{productName}</h1>
+    <img
+          src={product.imageAccessNumber}
+          alt="Product Image"
+          className="image-resize" // Apply CSS class for image resizing
+        />
     </div>
+
+  <div className="back" onClick={flipCard}>
+  <h1>{productName}</h1>
+    <div className="backInfo">
+      <div className="backTextWrapper">
+      <p>{description}</p>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+</div>
+    </>
+
   );
 };
 
