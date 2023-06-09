1970a7d6f4d546e819f3846845cf5d31ee75db41
ADerangedTeddyBear <cunhivu@gmail.com>
2023-06-09 15:13:38 -0700

    Added Desktop View

--- the-directory_react_frontend/src/components/Card.js
+++ the-directory_react_frontend/src/components/Card.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../assets/styles/Card.css';
 import TestImage from '../assets/images/testPicture.jpg';
 import '../../src/assets/styles/W3.css'
+import { BrowserView, MobileView } from 'react-device-detect';
 const Card = ({ product }) => {
   const [isFlipped, setIsFlipped] = useState(false);
   const flipCard = () => {
@@ -10,41 +11,81 @@ const Card = ({ product }) => {
 
   return (
     <>
+    <BrowserView>
+      <div className="card-grid card-Desktop">
+
+        <div className={`card ${isFlipped ? "flip" : ""}`}>
+
+          <div className="front" onClick={flipCard}>
+            <h2 className='cardHeaderBorderDesktop'>{product.productName}</h2>
+            <img
+            src={product.imageAccessNumber}
+            alt="Product Image"
+            className="image-resize" // Apply CSS class for image resizing
+            />
+          </div>
+
+          <div className="back" onClick={flipCard}>
+            <h2 className='cardHeaderBorderDesktop'>{product.productName}</h2>
+            <div className="backInfoDesktop, cardContentBorderDesktop">
+              <div className="backTextWrapper">
+                <p>{product.description}</p>
+                {product.color !== "" && (
+                <p>Color:{product.color}</p>
+                )}
+                {product.price !== "" && (
+                <p>Price: {product.price}</p>
+                )}
+                {product.madeIn !== "" && (
+                <p>Made In:{product.madeIn}</p>
+                )}
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </BrowserView>
+
+    <MobileView>
+      <div className="card-grid">
+
+        <div className={`card ${isFlipped ? "flip" : ""}`}>
+
+          <div className="front" onClick={flipCard}>
+            <h2 className='cardHeaderBorder'>{product.productName}</h2>
+            <img
+            src={product.imageAccessNumber}
+            alt="Product Image"
+            className="image-resize" // Apply CSS class for image resizing
+            />
+          </div>
+
+          <div className="back" onClick={flipCard}>
+            <h2 className='cardHeaderBorder'>{product.productName}</h2>
+            <div className="backInfo, cardContentBorder">
+              <div className="backTextWrapper">
+                <p>{product.description}</p>
+                {product.color !== "" && (
+                <p>Color:{product.color}</p>
+                )}
+                {product.price !== "" && (
+                <p>Price: {product.price}</p>
+                )}
+                {product.madeIn !== "" && (
+                <p>Made In:{product.madeIn}</p>
+                )}
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </MobileView>
 {/* My version below */}
-    <div className="card-grid">
-
-<div className={`card ${isFlipped ? "flip" : ""}`}>
-
-  <div className="front" onClick={flipCard}>
-    <h2 className='cardHeaderBorder'>{product.productName}</h2>
-    <img
-      src={product.imageAccessNumber}
-      alt="Product Image"
-      className="image-resize" // Apply CSS class for image resizing
-    />
-    </div>
-
-  <div className="back" onClick={flipCard}>
-  <h2 className='cardHeaderBorder'>{product.productName}</h2>
-    <div className="backInfo, cardContentBorder">
-      <div className="backTextWrapper">
-      <p>{product.description}</p>
-      {product.color !== "" && (
-      <p>Color:{product.color}</p>
-      )}
-      {product.price !== "" && (
-      <p>Price: {product.price}</p>
-      )}
-      {product.madeIn !== "" && (
-      <p>Made In:{product.madeIn}</p>
-      )}
-</div>
-    </div>
-  </div>
-
-</div>
-
-</div>
+ 
     </>
 
   );
--- the-directory_react_frontend/src/pages/Layout.js
+++ the-directory_react_frontend/src/pages/Layout.js
@@ -3,13 +3,15 @@ import Nav from "../components/page_components/Nav";
 import logo from '../assets/images/crocLogo.png';
 import '../assets/styles/W3.css';
 import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect'; 
+import 'reactjs-popup/dist/index.css';
+import Popup from "reactjs-popup";
 
 
 const Layout = () => {
   return (
     <>
     <BrowserView>
-      <br/>
+      {/* <br/>
       <h1>This webpage is designed for the future scope of the Admin side, providing them with the ability to add, remove, and update their products.</h1>
 
       <br/>
@@ -26,7 +28,11 @@ const Layout = () => {
       <h4>2. In the menu bar at the top of the screen, click on "Develop".</h4>
       <h4>3. If you do not see the "Develop" menu option, go to "Safari" - "Preferences" - "Advanced" and enable the checkbox labeled "Show Develop menu in menu bar".</h4>
       <h4>4. In the "Develop" menu, hover your cursor over "User Agent" and select the desired mobile device from the available options. You can choose from various models of iPhone, iPad, and iPod Touch.</h4>
-      <h4>5. Safari will automatically reload the webpage, presenting it in a simulated environment that mimics the selected mobile device's screen size and behavior.</h4>
+      <h4>5. Safari will automatically reload the webpage, presenting it in a simulated environment that mimics the selected mobile device's screen size and behavior.</h4> */}
+      <h1 className="Desktop-Title-Label">The Directory</h1>
+      {/* <img src={logo} className="App-logo-alt" alt="logo" /> */}
+      <Nav />
+      <Outlet />
     </BrowserView>
 
     <MobileView>
